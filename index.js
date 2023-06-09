fcdffc4f841f26960e8f8653f7ee639b5a1ccc92
Arpita-Malaker <arpita.uiu@gmail.com>
2023-06-09 12:39:50 +0600

    verify admin

--- index.js
+++ index.js
@@ -64,7 +64,22 @@ app.post('/jwt', (req, res) => {
 
 })
 
-app.get('/users', verifyJWT, async(req,res)=>{
+// verify admin before jwt
+
+
+const verifyAdmin = async(req,res,next)=>{
+  const email = req.decoded.email;
+  const query ={email: email};
+  const user = await usersCollection.findOne(query);
+  if(user?.role !== 'admin'){
+    return res.status(403).send({error:true,message:'forbidden message'})
+
+  }
+  next();
+}
+////
+
+app.get('/users', verifyJWT,verifyAdmin, async(req,res)=>{
   const result = await usersCollection.find().toArray();
   res.send(result);
 })
